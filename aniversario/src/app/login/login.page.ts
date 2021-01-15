b7727e7902728f1bb37e7809e98e6d9a7e4ab12d
Jaqueline <contato@girafa.digital>
2021-01-15 11:11:58 -0300

    estrutura do layout da tela de login

--- aniversario/src/app/login/login.page.ts
+++ aniversario/src/app/login/login.page.ts
@@ -1,4 +1,3 @@
-import { FirebaseAuthentication } from '@ionic-native/firebase-authentication';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,26 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  email: any;
-  password: any;
-  action = 'Ok';
-
-  constructor(
-    private firebaseAuth: FirebaseAuthentication
-  ) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  async loginEmailSenha() {
-    // const result = await this.firebaseAuth.createUserWithEmailAndPassword("my@mail.com", "pa55w0rd")
-    //   .then((res: any) => console.log(res))
-    //   .catch((error: any) => console.error(error));
-
-  //  console.log(result)
-
-
-
+  async loginEmailSenha(){
+    
   }
 
 }
